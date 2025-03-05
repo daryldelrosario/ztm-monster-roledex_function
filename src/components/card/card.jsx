0422348f1e9282c5593846518052588dcf32653a
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-03-05 16:33:55 -0500

    complete card refactor from class to function component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,6 +1,7 @@
 import './card.css';
 
-const Card = ({ id, name, email }) => {
+const Card = ({ monster }) => {
+  const { id, name, email } = monster;
 
   return (
     <div className='card-container' key={id}>
@@ -13,22 +14,5 @@ const Card = ({ id, name, email }) => {
     </div>
   )
 }
-
-// class Card extends Component {
-//   render() {
-//     const {id, name, email} =this.props.monster;
-
-//     return (
-//       <div className='card-container' key={id}>
-//         <img 
-//           src={`https://robohash.org/${id}?set=set2&size=180x180`} 
-//           alt={`monster ${name}`} 
-//         />
-//         <h2>{name}</h2>
-//         <p>{email}</p>
-//       </div>
-//     );
-//   }
-// }
 
 export default Card;
