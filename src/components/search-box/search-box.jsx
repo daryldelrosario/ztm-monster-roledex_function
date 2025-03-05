8048e0ca594f3706041e9817349e738fcd03c9a4
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-03-05 16:37:34 -0500

    complete refactor for searchbox class component to function component

--- src/components/search-box/search-box.jsx
+++ src/components/search-box/search-box.jsx
@@ -1,13 +1,13 @@
 import './search-box.css';
 
-const SearchBox = () => {
+const SearchBox = ({ className, placeholder, onChangeHandler}) => {
 
   return (
     <input 
-      className={`search-box ${this.props.className}`}
+      className={`search-box ${className}`}
       type='search' 
-      placeholder={this.props.placeholder}
-      onChange={this.props.onChangeHandler}
+      placeholder={placeholder}
+      onChange={onChangeHandler}
     />
   )
 }
