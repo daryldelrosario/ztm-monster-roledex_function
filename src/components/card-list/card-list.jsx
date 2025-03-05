4fdbc67e58ca21732889ac099e64c79e62115794
Daryl del Rosario <daryldelrosario.is@gmail.com>
2025-03-05 16:27:52 -0500

    refactor card-list from class to functional component

--- src/components/card-list/card-list.jsx
+++ src/components/card-list/card-list.jsx
@@ -1,23 +1,40 @@
 import Card from '../card/card';
 import './card-list.css';
 
-class CardList extends Component {
-  
-  render() {
-    const { monsters } = this.props;
+const CardList = () => {
+  const { monsters } = this.props;
 
-    return (
-      <>
-        <div className='card-list'>
-          {monsters.map((monster) => {
-            return (
-              <Card monster={monster} />
-            );
-          })}
-        </div>
-      </>
-    )
-  }
+  return (
+    <>
+      <div className='card-list'>
+        {monsters.map((monster) => {
+          return (
+            <Card monster={monster} />
+          )
+        })}
+
+      </div>
+    </>
+  )
 }
+
+// class CardList extends Component {
+  
+//   render() {
+//     const { monsters } = this.props;
+
+//     return (
+//       <>
+//         <div className='card-list'>
+//           {monsters.map((monster) => {
+//             return (
+//               <Card monster={monster} />
+//             );
+//           })}
+//         </div>
+//       </>
+//     )
+//   }
+// }
 
 export default CardList;
